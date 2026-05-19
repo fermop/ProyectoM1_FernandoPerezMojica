6df01ac667089a96fefc08ac0295066b2c0fbc07
Fernando Pérez Mojica <fernandoperezmojica@gmail.com>
2026-05-18 20:50:51 -0600

    fix: corrige ícono de copiar a uno responsivo dependiendo del color generado

--- main.js
+++ main.js
@@ -70,7 +70,7 @@ document.addEventListener('DOMContentLoaded', function() {
     divColor.innerHTML = `
       <p class="color__formato-texto">${colorGenerado}</p>
       <button type="button" class="boton-copiar animacion-click cursor-pointer" aria-label="Copiar color al portapapeles">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-copy-icon lucide-copy"><rect width="14" height="14" x="8" y="8" rx="2" ry="2"/><path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"/></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" style="color: ${esOscuro ? 'white' : 'black'};" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-copy-icon lucide-copy"><rect width="14" height="14" x="8" y="8" rx="2" ry="2"/><path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"/></svg>
       </button>
     `;
 
@@ -112,7 +112,7 @@ botonGenerar.addEventListener('click', function(e) {
     divColor.innerHTML = `
       <p class="color__formato-texto">${colorGenerado}</p>
       <button type="button" class="boton-copiar animacion-click cursor-pointer" aria-label="Copiar color al portapapeles">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-copy-icon lucide-copy"><rect width="14" height="14" x="8" y="8" rx="2" ry="2"/><path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"/></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" style="color: ${esOscuro ? 'white' : 'black'};"  width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-copy-icon lucide-copy"><rect width="14" height="14" x="8" y="8" rx="2" ry="2"/><path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"/></svg>
       </button>
     `;
 
