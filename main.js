3688ff17df2ba21e52bbfae43eb2bd02c343d986
Fernando Pérez Mojica <fernandoperezmojica@gmail.com>
2026-05-19 10:20:18 -0600

    feat: agrega función para error al copiar color al portapapeles

--- main.js
+++ main.js
@@ -74,6 +74,29 @@ async function copiarAlPortapapeles(text) {
     }, 1000);
   } catch (err) {
     console.error('Error al copiar: ', err);
+
+    const toast = document.createElement('div')
+    toast.classList.add('toast')
+    toast.classList.add('error')
+    toast.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-circle-x-icon lucide-circle-x"><circle cx="12" cy="12" r="10"/><path d="m15 9-6 6"/><path d="m9 9 6 6"/></svg>
+      <span>Error al copiar al portapapeles</span>
+    `
+
+    toastContenedor.appendChild(toast)
+
+    setTimeout(() => {
+      toast.classList.add('show')
+    }, 10);
+
+    setTimeout(() => {
+      toast.classList.remove('show')
+      toast.addEventListener('transitionend', () => {
+        if (toast.parentElement) {
+          toast.remove()
+        }
+      })
+    }, 1000);
   }
 }
 
