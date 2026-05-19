dbbaf8c7c54ddf56fb956f455cdc9bb8f4c9f756
Fernando Pérez Mojica <fernandoperezmojica@gmail.com>
2026-05-18 20:42:15 -0600

    feat: agrega clase de utilidad para cursor-pointer

--- main.js
+++ main.js
@@ -69,7 +69,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Insertar el HTML interno (texto y botón)
     divColor.innerHTML = `
       <p class="color__formato-texto">${colorGenerado}</p>
-      <button type="button" class="boton-copiar animacion-click" aria-label="Copiar color al portapapeles">
+      <button type="button" class="boton-copiar animacion-click cursor-pointer" aria-label="Copiar color al portapapeles">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-copy-icon lucide-copy"><rect width="14" height="14" x="8" y="8" rx="2" ry="2"/><path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"/></svg>
       </button>
     `;
@@ -111,7 +111,7 @@ botonGenerar.addEventListener('click', function(e) {
     // Insertar el HTML interno (texto y botón)
     divColor.innerHTML = `
       <p class="color__formato-texto">${colorGenerado}</p>
-      <button type="button" class="boton-copiar" aria-label="Copiar color al portapapeles">
+      <button type="button" class="boton-copiar animacion-click cursor-pointer" aria-label="Copiar color al portapapeles">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-copy-icon lucide-copy"><rect width="14" height="14" x="8" y="8" rx="2" ry="2"/><path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"/></svg>
       </button>
     `;
