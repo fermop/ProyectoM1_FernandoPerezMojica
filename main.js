3fb7cc5280cebb8c4168a6d1649a913b66ef0cc3
Fernando Pérez Mojica <fernandoperezmojica@gmail.com>
2026-05-18 20:55:51 -0600

    fix: estandariza estilizado por clases de utilidad en vez de style directo

--- main.js
+++ main.js
@@ -64,13 +64,12 @@ document.addEventListener('DOMContentLoaded', function() {
     const divColor = document.createElement('div');
     divColor.classList.add('wrapper-color');
     divColor.style.backgroundColor = colorGenerado;
-    divColor.style.color = esOscuro ? 'white' : 'black';
 
     // Insertar el HTML interno (texto y botón)
     divColor.innerHTML = `
-      <p class="color__formato-texto">${colorGenerado}</p>
-      <button type="button" class="boton-copiar animacion-click cursor-pointer" aria-label="Copiar color al portapapeles">
-        <svg xmlns="http://www.w3.org/2000/svg" style="color: ${esOscuro ? 'white' : 'black'};" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-copy-icon lucide-copy"><rect width="14" height="14" x="8" y="8" rx="2" ry="2"/><path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"/></svg>
+      <p class="color__formato-texto ${esOscuro && 'es-oscuro'}">${colorGenerado}</p>
+      <button type="button" class="boton-copiar animacion-click cursor-pointer ${esOscuro && 'es-oscuro'}" aria-label="Copiar color al portapapeles">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-copy-icon lucide-copy"><rect width="14" height="14" x="8" y="8" rx="2" ry="2"/><path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"/></svg>
       </button>
     `;
 
@@ -106,13 +105,12 @@ botonGenerar.addEventListener('click', function(e) {
     const divColor = document.createElement('div');
     divColor.classList.add('wrapper-color');
     divColor.style.backgroundColor = colorGenerado;
-    divColor.style.color = esOscuro ? 'white' : 'black';
 
     // Insertar el HTML interno (texto y botón)
     divColor.innerHTML = `
-      <p class="color__formato-texto">${colorGenerado}</p>
-      <button type="button" class="boton-copiar animacion-click cursor-pointer" aria-label="Copiar color al portapapeles">
-        <svg xmlns="http://www.w3.org/2000/svg" style="color: ${esOscuro ? 'white' : 'black'};"  width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-copy-icon lucide-copy"><rect width="14" height="14" x="8" y="8" rx="2" ry="2"/><path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"/></svg>
+      <p class="color__formato-texto ${esOscuro && 'es-oscuro'}">${colorGenerado}</p>
+      <button type="button" class="boton-copiar animacion-click cursor-pointer ${esOscuro && 'es-oscuro'}" aria-label="Copiar color al portapapeles">
+        <svg xmlns="http://www.w3.org/2000/svg"  width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-copy-icon lucide-copy"><rect width="14" height="14" x="8" y="8" rx="2" ry="2"/><path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"/></svg>
       </button>
     `;
 
