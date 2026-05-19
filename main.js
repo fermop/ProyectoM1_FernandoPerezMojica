caa6c3dfd21e40cbc727638cb273cef42821d2ed
Fernando Pérez Mojica <fernandoperezmojica@gmail.com>
2026-05-18 19:47:27 -0600

    feat: agrega funcionalidad para generar colores al visitar página y con botón (solo hsl)

---
+++ main.js
@@ -0,0 +1,101 @@
+// Selectores
+const botonGenerar = document.getElementById('boton-generar')
+const selectCantidad = document.getElementById('select-cantidad-paleta')
+const selectFormatoColor = document.getElementById('select-formato-color')
+
+const wrapperPaletaColores = document.getElementById('wrapper-paleta-colores')
+const wrapperColors = document.querySelectorAll('.wrapper-color')
+const colorFormatoTextos = document.querySelectorAll('.color__formato-texto')
+
+// Variables y estados
+
+
+// Funciones
+const generarColorHsl = function() {
+  const h = Math.floor(Math.random() * 360)
+  const s = Math.floor(Math.random() * 100)
+  const l = Math.floor(Math.random() * 100)
+
+  const colorHslGenerado = `hsl(${h} ${s}% ${l}%)`
+
+  // Determinar si el color generado es oscuro para cambiar el color de letra
+    // Se determina si el lightness es menor a 50%
+  const esOscuro = l < 50
+
+  return [colorHslGenerado, esOscuro]
+}
+
+// Eventos
+  // Al visitar la página, renderizar colores por defecto
+document.addEventListener('DOMContentLoaded', function() {
+  // 1. Obtener los valores de los selects
+  const cantidadDeColoresAGenerar = Number(selectCantidad.value);
+  const formatoDeColoresAGenerar = selectFormatoColor.value;
+
+  // 2. Limpiar el contenedor principal antes de agregar los nuevos
+  wrapperPaletaColores.innerHTML = '';
+
+  // 3. Iterar y crear los nuevos elementos
+  for (let i = 0; i < cantidadDeColoresAGenerar; i++) {  
+    const [colorGenerado, esOscuro] = generarColorHsl();
+
+    // Crear el contenedor principal del color
+    const divColor = document.createElement('div');
+    divColor.classList.add('wrapper-color');
+    divColor.style.backgroundColor = colorGenerado;
+    divColor.style.color = esOscuro ? 'white' : 'black';
+
+    // Insertar el HTML interno (texto y botón)
+    divColor.innerHTML = `
+      <p class="color__formato-texto">${colorGenerado}</p>
+      <button type="button" class="btn-copiar" aria-label="Copiar color al portapapeles">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-copy-icon lucide-copy"><rect width="14" height="14" x="8" y="8" rx="2" ry="2"/><path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"/></svg>
+      </button>
+    `;
+
+    // 4. Inyectar en el DOM
+    wrapperPaletaColores.appendChild(divColor);
+  }
+});
+
+
+botonGenerar.addEventListener('click', function(e) {
+  e.preventDefault();
+
+  // 1. Obtener los valores de los selects
+  const cantidadDeColoresAGenerar = Number(selectCantidad.value);
+  const formatoDeColoresAGenerar = selectFormatoColor.value;
+
+  // 2. Limpiar el contenedor principal antes de agregar los nuevos
+  wrapperPaletaColores.innerHTML = '';
+
+  // 3. Iterar y crear los nuevos elementos
+  for (let i = 0; i < cantidadDeColoresAGenerar; i++) {
+    let colorGenerado;
+    let esOscuro;
+    
+    // Generar los datos del color
+    if (formatoDeColoresAGenerar === 'hsl') {
+      [colorGenerado, esOscuro] = generarColorHsl();
+    } else if (formatoDeColoresAGenerar === 'hex') {
+      // [colorGenerado, esOscuro] = generarColorHex();
+    }
+
+    // Crear el contenedor principal del color
+    const divColor = document.createElement('div');
+    divColor.classList.add('wrapper-color');
+    divColor.style.backgroundColor = colorGenerado;
+    divColor.style.color = esOscuro ? 'white' : 'black';
+
+    // Insertar el HTML interno (texto y botón)
+    divColor.innerHTML = `
+      <p class="color__formato-texto">${colorGenerado}</p>
+      <button type="button" class="btn-copiar" aria-label="Copiar color al portapapeles">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-copy-icon lucide-copy"><rect width="14" height="14" x="8" y="8" rx="2" ry="2"/><path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"/></svg>
+      </button>
+    `;
+
+    // 4. Inyectar en el DOM
+    wrapperPaletaColores.appendChild(divColor);
+  }
+});
